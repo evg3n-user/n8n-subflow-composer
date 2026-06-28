6d79c81688acd8a7ffecab790759bdd2162ce3d4
evg3n-user <ievgen.gniedko@gmail.com>
2026-06-28 14:05:15 +0000

    feat(subflow-composer): add stub module for reusable subflow composition

---
+++ packages/frontend/editor-ui/src/features/subflowComposer/module.descriptor.ts
@@ -0,0 +1,8 @@
+import { type FrontendModuleDescription } from '@/app/moduleInitializer/module.types';
+
+export const SubflowComposerModule: FrontendModuleDescription = {
+	id: 'subflow-composer',
+	name: 'Subflow Composer',
+	description: 'Create reusable subflows from node selections',
+	icon: 'cubes',
+};
